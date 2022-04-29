e1fa0059eb70d49c34969c51f57da3135370d2b6
Nilly (iago) TheL0L <97253814+NillyTheL0L@users.noreply.github.com>
2022-04-29 17:30:51 -0300

    Add files via upload

---
+++ src/aga.cr
@@ -0,0 +1,55 @@
+class Aga
+
+def self.start
+"<!DOCTYPE html>"
+end
+
+def self.end
+"</html>"
+end
+
+def self.heo
+"<head>"
+end
+
+def self.hec
+"</head>"
+end
+
+def self.h1o
+"<h1>"
+end
+
+def self.h1c
+"</h1>"
+end
+
+def self.po
+"<p>"
+end
+
+def self.pc
+"</p>"
+end
+
+def self.boo
+"<body>"
+end
+
+def self.boc
+"</body>"
+end
+
+def self.img
+"<img>"
+end
+
+def self.sto
+"<style>"
+end
+
+def self.stc
+"</style>"
+end
+
+end
